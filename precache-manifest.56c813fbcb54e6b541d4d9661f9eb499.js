f00667b43d2148473aa1784147b5e1819fac9e84
Troyer Morse <morsecode2000@gmail.com>
2020-03-02 11:18:27 -0600

    Updates

---
+++ precache-manifest.56c813fbcb54e6b541d4d9661f9eb499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7974b1c3edfb276542a8f4b6f06b02b",
+    "url": "/linesOfAction/index.html"
+  },
+  {
+    "revision": "986d1fde3f8220793854",
+    "url": "/linesOfAction/static/css/main.1b14ccba.chunk.css"
+  },
+  {
+    "revision": "dd473e454ce185968d1b",
+    "url": "/linesOfAction/static/js/2.aeb40133.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/linesOfAction/static/js/2.aeb40133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986d1fde3f8220793854",
+    "url": "/linesOfAction/static/js/main.18b6a1a4.chunk.js"
+  },
+  {
+    "revision": "380db843390c523641dc",
+    "url": "/linesOfAction/static/js/runtime-main.3e923eb6.js"
+  }
+]);
